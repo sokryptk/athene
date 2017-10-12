36a4ec173e49b759592425e07fcb6cc1e4e6591c
krypticallusion <krypticallusion@gmail.com>
2017-10-12 04:51:20 +0000

    Remove one group till breakpoint is achieved
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ cp -rf $AKdir/$KernelZip $PWD/$KernelZip
 
 
 curl -F chat_id="-1001143664346" -F document=@"$PWD/$KernelZip" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
-curl -F chat_id="-1001079512482" -F document=@"$PWD/$KernelZip" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+# curl -F chat_id="-1001079512482" -F document=@"$PWD/$KernelZip" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
